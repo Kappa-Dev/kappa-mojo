b2b7e20bd5378402e9f8c3fbeaa4b0584a0aa1e9
Yarden Katz <yarden.katz@gmail.com>
2019-05-28 11:36:05 -0400

    adding documentation and plots

--- analysis/simple_chain.py
+++ analysis/simple_chain.py
@@ -1,6 +1,67 @@
+##
 ## Analyze simple chain model
-import mojo_utils
+##
+## Author: Yarden Katz
+##
+import matplotlib
+import matplotlib.pylab as plt
+import seaborn as sns
+import numpy as np
 
-info = mojo_utils.get_model_info("simple_chain")
-print("Model information for simple chain: ")
-print(info)
+import mojo_utils
+import kappy
+import kappy.kappa_snap as kappa_snap
+
+def get_chain_hist(snap_obj, node_type="C"):
+    """
+    Get chain histogram. 
+    Assume that the rest are forming chains, meaning
+    the number of components is equal to the chain length
+    """
+    num_blocks_per_complex = []
+    for abundance, comp in snap_obj.complexes:
+        agent_types = comp.get_agent_types()
+        if node_type not in agent_types:
+            # skip over complexes not made up of node type
+            continue
+        # assume that the rest are forming chains, meaning
+        # the number of components is equal to the chain length
+        num_blocks = agent_types[node_type]
+        # take abundance into account
+        num_blocks_per_complex.append([num_blocks] * abundance)
+    # flatten list
+    num_blocks_per_complex = [elt for sublist in num_blocks_per_complex \
+                              for elt in sublist]
+    num_blocks_per_complex.sort()
+    num_blocks_per_complex = np.array(num_blocks_per_complex)
+    complex_sizes = \
+      np.arange(num_blocks_per_complex.min(), num_blocks_per_complex.max() + 1)
+    histogram, histogram_bins = \
+      np.histogram(num_blocks_per_complex, complex_sizes)
+    return {"num_blocks_per_complex": num_blocks_per_complex,
+            "histogram": histogram,
+            "histogram_bins": histogram_bins[0:-1]}
+
+def main():
+    info = mojo_utils.get_model_info("simple_chain")
+    print("Model information for simple chain: ")
+    print(info)
+    # load Kappa snapshot (in JSON format)
+    snap_obj = kappa_snap.KappaSnapshot(from_fname=info["snap_fname"])
+    snapshot_time = snap_obj["snapshot_time"]
+    # calculate number of chains we have
+    chain_hist = get_chain_hist(snap_obj)
+    # make plot of chain length distribution
+    plt.figure(figsize=(4,4))
+    sns.set_style("ticks")
+    # plot without binning
+    #plt.bar(chain_hist["histogram_bins"], chain_hist["histogram"], color="k")
+    # plot with binning
+    plt.hist(chain_hist["num_blocks_per_complex"], color="k", bins=30)
+    plt.ylabel("Number of complexes", fontsize=10)
+    plt.xlabel("Chain length", fontsize=10)
+    plt.title("Chain length distribution (at t = %.1f)" %(snapshot_time))
+    plt.savefig("../plots/simple_chain.png")
+
+if __name__ == "__main__":
+    main()
--- docs/simple_chain.md
+++ docs/simple_chain.md
@@ -1,9 +1,19 @@
-Simple chain model
-====================
+Simple chain polymerization model
+====================================
 
 **Author:** Yarden Katz
 
-Simple model where ``C`` agents can polymerize into chains.
+Simple model where ``C`` agents can polymerize into chains ([simple_chain.ka](../models/simple_chain.ka)). The main rule is:
+
+```
+'form_chain' C(x,y[.]),C(x[.],y[.]) <-> C(x,y[1]),C(x[1],y[.]) @ 'chain_bind_k', 'chain_unbind_k'
+```
+
+This rule states that ``C`` agents can polymerize one at a time; meaning a free ``C`` can be added to the end of an existing chain (sometimes referred to as "chain-growth polymerization"). 
+
+If we run this model for a while and obtain a snapshot from Kappa, we get the following distribution of chain lengths:
+
+![Chain length distribution](../plots/simple_chain.png "Chain length distribution")
 
 
 
